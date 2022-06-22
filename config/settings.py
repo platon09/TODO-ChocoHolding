3572b8182550d82d289444294cf9d34d860f0da9
platon09 <nis.platon@gmail.com>
2022-06-22 17:08:42 +0600

    added api docs

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "todo.User"
+
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
