5a69b927b82e1716b2e97fd8161a6d5610146df1
platon09 <nis.platon@gmail.com>
2022-06-22 17:08:36 +0600

    added api docs

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.utils.translation import gettext_lazy as _
+from rest_framework.documentation import include_docs_urls
 
 
 urlpatterns = [
@@ -12,4 +14,11 @@ urlpatterns = [
     path('api/v0/', include([
         path('', include('todo.api.urls'),)
     ])),
+    path('api/docs/', include_docs_urls(
+        title='Employee API documentation'
+    )),
 ]
+
+admin.site.site_header = _("TODO Administration")
+admin.site.site_title = _("TODO Admin Portal")
+admin.site.index_title = _("Welcome to TODO Portal")
