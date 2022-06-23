07139de3b800916d1b70b1e23d944053aa144a80
platon09 <nis.platon@gmail.com>
2022-06-23 18:25:55 +0600

    added jwt token authentication

--- config/urls.py
+++ config/urls.py
@@ -7,18 +7,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.utils.translation import gettext_lazy as _
 from rest_framework.documentation import include_docs_urls
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
+    # admin panel
     path('admin/', admin.site.urls),
-    path('api/v0/', include([
-        path('', include('todo.api.urls'),)
-    ])),
+    # to redirect TO'DO endoints
+    path('api/v0/', include('todo.api.urls')),
+    # to get access, refresh token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # to refresh access token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # api docs
     path('api/docs/', include_docs_urls(
         title='Employee API documentation'
     )),
 ]
 
+# set header, title of site && title of index
 admin.site.site_header = _("TODO Administration")
 admin.site.site_title = _("TODO Admin Portal")
 admin.site.index_title = _("Welcome to TODO Portal")
